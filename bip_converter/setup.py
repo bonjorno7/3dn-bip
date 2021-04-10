c039784671a6c64d992e3ab5688475e5e5a0e3be
bonjorno7 <jorijndegraaf@gmail.com>
2021-04-10 16:45:17 +0200

    Create t3dn_bip_converter
    This relies on Pillow.
    I can't find PIL on PyPy or GitHub, so I'm just going to assume people don't have it installed (if they did, Pillow would fail to install).

---
+++ bip_converter/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='t3dn-bip-converter',
+    packages=find_packages(),
+    install_requires=['Pillow'],
+)
---
+++ bip_converter/t3dn_bip_converter/__init__.py
@@ -0,0 +1,3 @@
+'''Convert between BIP files and various image formats.'''
+
+__version__ = '0.0.1'
