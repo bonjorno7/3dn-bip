3cb725be5e77e16287fac5b3e0cd72ea225cd67b
bonjorno7 <jorijndegraaf@gmail.com>
2021-04-11 00:51:07 +0200

    Create t3dn_bip/load.py
    This function supports BIP, and if PIL is installed, other formats too.

---
+++ bip/t3dn_bip/load.py
@@ -0,0 +1,53 @@
+from typing import Union, Tuple, List
+from pathlib import Path
+from zlib import decompress
+from array import array
+
+try:
+    from PIL import Image
+except:
+    _SUPPORT_PIL = False
+else:
+    _SUPPORT_PIL = True
+
+
+def load_file(src: Union[str, Path]) -> Tuple[int, int, List[int]]:
+    '''Load image preview data from file.
+
+    Args:
+        src: The input file path.
+
+    Returns:
+        The width, height, and pixels.
+
+    Raises:
+        AssertionError: If pixel data type is not 32 bit.
+        ValueError: If file is not BIP and PIL is not found.
+    '''
+    with open(src, 'rb') as bip:
+        magic = bip.read(4)
+
+        if magic == b'BIP1':
+            width = int.from_bytes(bip.read(2), 'big')
+            height = int.from_bytes(bip.read(2), 'big')
+            data = decompress(bip.read())
+
+            pixels = array('i', data)
+            assert pixels.itemsize == 4, '32 bit type required for pixels'
+
+            return width, height, pixels
+
+    if _SUPPORT_PIL:
+        with Image.open(src) as image:
+            image = image.transpose(Image.FLIP_TOP_BOTTOM)
+            image = image.convert('RGBA')
+
+            width, height = image.size
+            data = image.tobytes()
+
+            pixels = array('i', data)
+            assert pixels.itemsize == 4, '32 bit type required for pixels'
+
+            return width, height, pixels
+
+    raise ValueError('input is not a supported file format')
