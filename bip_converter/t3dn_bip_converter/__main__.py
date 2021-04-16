3cbf4ff7fd56a31c147637f59a9f9a7bcf47d9bf
bonjorno7 <jorijndegraaf@gmail.com>
2021-04-16 02:58:30 +0200

    Use positional arguments for t3dn_bip_converter
    Turns out it is possible, just have to set nargs to '?', which means zero or one.

--- bip_converter/t3dn_bip_converter/__main__.py
+++ bip_converter/t3dn_bip_converter/__main__.py
@@ -3,8 +3,8 @@ from .convert import convert_file
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='convert between BIP and other formats')
-    parser.add_argument('--src', type=str, required=True, help='input path')
-    parser.add_argument('--dst', type=str, required=False, help='output path')
+    parser.add_argument('src', type=str, help='input path')
+    parser.add_argument('dst', type=str, nargs='?', help='output path')
 
     args = parser.parse_args()
     convert_file(args.src, args.dst)
