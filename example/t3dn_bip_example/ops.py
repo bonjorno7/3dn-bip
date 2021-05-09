514583ae978d54c4549ec7880c731d3568714a40
bonjorno7 <43251584+bonjorno7@users.noreply.github.com>
2021-05-09 18:28:01 +0200

    Finetune the code (#31)
    * Remove unnecessary newline
    
    YAPF says it needs to go.
    
    * Add execute method to dummy operator
    
    Otherwise it complains about it in the console.
    
    * Attempt to tidy up the code
    
    formats:
    BIP and PIL formats share the same class, I think it's fine.
    Max magic length is calculated here instead of every time it's used.
    Tests are now run here instead of in utils.
    Unsupported formats is defined here as well.
    
    utils:
    Renamed _try_import_pillow to just _import_pillow, and gave it a docstring.
    It doesn't return anything, and should only be called if Image is None, which support_pillow() does.
    can_load is a bit more readable now I think.
    Magic for BIP2 is stored in BIP_FORMATS instead of as its own variable.
    
    previews:
    Reduced the amount of spaces before warning messages, just so that the print statements would fit on their lines.
    Split the unsupported print into two strings.
    
    * Ensure load works for new documents too.
    
    * Remove unnecessary global keyword
    
    Fair enough, we're not setting Image here.
    
    * Add _BIPFormat class
    
    I think reusing the class was fine but Niklas disagrees, and suggested we make the dict just contain magics.
    Not a fan of that myself, so I'll add a separate class for BIP formats then.
    
    * Except all exceptions just in case
    
    It's important that these places don't raise, so let's just except all exceptions I guess.
    
    * Only try to import Image if PIL is in sys.modules
    
    Otherwise BIP images take twice as long to load when Pillow isn't installed, because of all those try/except calls.
    
    * Read magic for BIP differently for consistency
    
    And then seek to the end of that magic, because the longest magic may be longer than the one for BIP2, and it's important that our offset is correct.
    
    Co-authored-by: Niklas Salmoukas <niklas@salmoukas.com>

--- example/t3dn_bip_example/ops.py
+++ example/t3dn_bip_example/ops.py
@@ -116,6 +116,9 @@ class T3DN_OT_bip_example_dummy(bpy.types.Operator):
     bl_description = 'Does nothing'
     bl_options = {'REGISTER', 'INTERNAL'}
 
+    def execute(self, context: bpy.types.Context) -> set:
+        return {'FINISHED'}
+
 
 classes = (
     T3DN_OT_bip_example_install_pillow,
