8caa61d96d4780b3bb6482e39cbfaf4305e62a9e
Niklas Salmoukas <niklas@salmoukas.com>
2021-05-09 19:52:00 -0700

    Add README.md to PyPI package.

--- bip/setup.py
+++ bip/setup.py
@@ -1,3 +1,22 @@
 from setuptools import setup, find_packages
 
-setup(name='t3dn-bip', packages=find_packages(), install_requires=[])
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='t3dn-bip',
+    author="3D Ninjas GmbH",
+    author_email="niklas@3dninjas.io",
+    description="3DN BIP allows blazingly fast image loads in Blender.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/3dninjas/3dn-bip",
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    license="GPLv3",
+    packages=find_packages(),
+    install_requires=[],
+)
